67728c293b3e02d77b2963695fcd05221bfec5c0
Ilya Shevchenko <Sevc_ph43@student.itstep.org>
2023-02-15 16:50:44 +0200

    Session storage/ authorization/registration

--- src/Authorization.js
+++ src/Authorization.js
@@ -1,12 +1,27 @@
+import axios, { Axios } from 'axios';
 import './Authorize.css';
 function Authoriz(){
     return(
         <div className='mainDiv'>
             <h1>Authorization</h1>
-            <input type="text" placeholder="Login"/>
-            <input style={{margin:'0px'}} type="password" placeholder="Password"/>
-            <button onClick={()=>{
-                alert('click');
+            <input id='login' type="text" placeholder="Login" />
+            <input id='pass' style={{ margin: '0px' }} type="password" placeholder="Password" />
+            <button onClick={() => {
+                axios({
+                    method: 'post',
+                    url: 'https://localhost:7031/api/Authentication/Login',
+                    data: {
+                        "userName": document.getElementById('login').value,
+                        "password": document.getElementById('pass').value
+                    },
+                    dataType: "dataType",
+                    headers: {
+                        'Accept': '*/*',
+                        'Content-Type': 'application/json'
+                    }
+                }).then(data => {
+                    sessionStorage.setItem('token', data['data']['token']);
+                });
             }}>Confirm</button>
         </div>
     );
--- src/index.js
+++ src/index.js
@@ -3,13 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import Authorize from './Authorization';
+import Registration from './Registration';
+
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <Authorize/>
+    {/* <Authorize/> */}
+    <Registration/>
   </React.StrictMode>
 );
 
